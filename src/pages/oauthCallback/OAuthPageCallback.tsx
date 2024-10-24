8cbec7138a56cc446f29b855d873bf494cd14e5e
Dmitrii Sakulin <elaydweb@gmail.com>
2024-10-24 23:00:11 +0300

    OAuthPageCallback fix useEffect + name change in button

--- src/pages/oauthCallback/OAuthPageCallback.tsx
+++ src/pages/oauthCallback/OAuthPageCallback.tsx
@@ -1,37 +1,33 @@
 import { axiosInstance } from "@/axios";
-import { useRef, useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuthStore, { isAuthSelector, setAuthSelector } from "@/store/auth";
 import { ITokenResponse } from "@/types";
 
 export const OAuthPageCallback = () => {
-  const called = useRef(false);
   const navigate = useNavigate();
   const setAuth = useAuthStore(setAuthSelector);
   const isAuth = useAuthStore(isAuthSelector);
 
   useEffect(() => {
-    (async () => {
-      if (isAuth === false) {
-        try {
-          if (called.current) return;
-          called.current = true;
-          const response = await axiosInstance.get<ITokenResponse>(
-            `/oauth/token${window.location.search}`
-          );
-          const tokens = response.data;
-          const { accessToken, refreshToken } = tokens;
-          localStorage.setItem("accessToken", accessToken);
-          localStorage.setItem("refreshToken", refreshToken);
-          setAuth(true);
-          navigate("/chat");
-        } catch (err) {
-          console.error(err);
-        }
-      } else if (isAuth === true) {
+    if (isAuth) {
+      navigate("/chat");
+      return;
+    }
+
+    axiosInstance
+      .get<ITokenResponse>(`/oauth/token${window.location.search}`)
+      .then((response) => {
+        const tokens = response.data;
+        const { accessToken, refreshToken } = tokens;
+        localStorage.setItem("accessToken", accessToken);
+        localStorage.setItem("refreshToken", refreshToken);
+        setAuth(true);
         navigate("/chat");
-      }
-    })();
-  }, [navigate, setAuth, isAuth]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [isAuth, navigate, setAuth]);
   return <></>;
 };
