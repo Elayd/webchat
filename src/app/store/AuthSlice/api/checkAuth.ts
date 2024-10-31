20ea706565be89798a1b7b6010e4d4cfd3613fbd
Dmitrii Sakulin <elaydweb@gmail.com>
2024-11-01 00:36:25 +0300

    add comment

--- src/app/store/AuthSlice/api/checkAuth.ts
+++ src/app/store/AuthSlice/api/checkAuth.ts
@@ -2,7 +2,7 @@ import { axiosInstance } from "@/shared/api";
 
 export const fetchAuthStatus = async () => {
   const accessToken = localStorage.getItem("accessToken");
-
+  // пока так, до userService
   return await axiosInstance.post(
     "http://localhost:8000/api/security/checkAuth",
     {},
