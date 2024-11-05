fed4a0f4ee479cf1e4d408ca542fbff5c70e8d55
Dmitrii Sakulin <elaydweb@gmail.com>
2024-11-05 20:56:16 +0300

    remove values from errorCodes enum

--- src/shared/enums/errorCodes.ts
+++ src/shared/enums/errorCodes.ts
@@ -2,8 +2,4 @@ export enum ErrorCodes {
   UserNotFound = 101,
   UserAlreadyExists = 102,
   InvalidCredentials = 103,
-  InvalidToken = 104,
-  InternalServerError = 106,
-  InvalidRequest = 107,
-  BadRequest = 110,
 }
