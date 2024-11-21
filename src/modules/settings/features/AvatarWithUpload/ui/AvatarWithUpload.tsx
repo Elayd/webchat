a2cfb7d92610a749d58e50ef8b05d4c513b1e9c6
Dmitrii Sakulin <elaydweb@gmail.com>
2024-11-21 19:07:09 +0300

    Add image for avatar preview to download

--- src/modules/settings/features/AvatarWithUpload/ui/AvatarWithUpload.tsx
+++ src/modules/settings/features/AvatarWithUpload/ui/AvatarWithUpload.tsx
@@ -1,8 +1,9 @@
-import { FC, memo } from "react";
+import { FC, memo, useState } from "react";
 
 import { Avatar } from "@/common/components/Avatar/Avatar";
 import { cn } from "@/common/utils/cn";
 
+import downloadIcon from "../../../assets/download.png";
 interface AvatarWithUploadProps {
   picture: string;
   handleChangeAvatar: (event: React.ChangeEvent<HTMLInputElement>) => void;
@@ -11,9 +12,35 @@ interface AvatarWithUploadProps {
 
 export const AvatarWithUpload: FC<AvatarWithUploadProps> = memo(
   ({ picture, handleChangeAvatar, className }) => {
+    const [showDownloadIcon, setShowDownloadIcon] = useState(false);
+
+    const hasNoPicture = !picture;
+
+    const handleMouseOver = () => {
+      if (!hasNoPicture) setShowDownloadIcon(true);
+    };
+
+    const handleMouseLeave = () => {
+      if (!hasNoPicture) setShowDownloadIcon(false);
+    };
+
     return (
       <>
-        <label className="cursor-pointer" htmlFor="avatar">
+        <label
+          className="cursor-pointer relative"
+          htmlFor="avatar"
+          onMouseOver={handleMouseOver}
+          onMouseLeave={handleMouseLeave}
+        >
+          {(hasNoPicture || showDownloadIcon) && (
+            <div className="absolute z-10 flex items-center justify-center w-[100%] h-[100%] rounded-full bg-white shadow-lg opacity-20">
+              <img
+                src={downloadIcon}
+                alt="Download icon"
+                className="w-[60%] h-[60%]"
+              />
+            </div>
+          )}
           <Avatar picture={picture} className={cn(className)} />
         </label>
 
